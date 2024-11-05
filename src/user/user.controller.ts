9e61d6e20ec9d35f081ba11e16c8208d19062749
GolamMuid <golammuid@gmail.com>
2024-11-05 21:54:00 +0600

    added separate jwt guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
+  // ? Guarding protected routes
+
+  //   @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@Req() req: Request) {
+    console.log({
+      user: req.user,
+    });
+    return req.user;
   }
 }
