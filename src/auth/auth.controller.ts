9882883fccbcf42bef3b463d6c0a675054e9bfd4
GolamMuid <golammuid@gmail.com>
2024-11-14 22:13:26 +0600

    added test and fixed imports

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
+import { AuthDto } from './dto/auth.dto';
+
 
 @Controller('auth')
 export class AuhtController {
