3c2d78c2621af08f96e0c2f3f2d9c9fcd2c3af00
GolamMuid <golammuid@gmail.com>
2024-11-16 21:15:48 +0600

    updated main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.getHttpAdapter().get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
